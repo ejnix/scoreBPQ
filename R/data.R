8897a80dccc04e1b0c68021d977e59d5119d87d5
ejnix <ejnix@iu.edu>
2021-09-20 13:23:55 -0400

    Added population norms tables and documentation

--- R/data.R
+++ R/data.R
@@ -9,3 +9,137 @@
 #'   ...
 #' }
 "test_data"
+
+#' 2020 Population Norms for BPQ Awareness
+#'
+#' A dataset containing population norms for BPQ Awareness, calculated from a sample collected in 2020
+#'
+#' @format A data frame with 105 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"ATable"
+
+#' 2018 Population Norms for BPQ Awareness
+#'
+#' A dataset containing population norms for BPQ Awareness, calculated from a sample collected in 2018
+#'
+#' @format A data frame with 105 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"ATable_2018"
+
+
+#' 2020 Population Norms for BPQ Combined Reactivity (Sub- and Supra-diaphragmatic)
+#'
+#' A dataset containing population norms for BPQ Combined Reactivity, calculated from a sample collected in 2020
+#'
+#' @format A data frame with 81 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"CRTable"
+
+#' 2018 Population Norms for BPQ Combined Reactivity (Sub- and Supra-diaphragmatic)
+#'
+#' A dataset containing population norms for BPQ Combined Reactivity, calculated from a sample collected in 2018
+#'
+#' @format A data frame with 81 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"CRTable_2018"
+
+
+#' 2020 Population Norms for BPQ Sub-diaphragmatic Reactivity
+#'
+#' A dataset containing population norms for BPQ Sub-diaphragmatic Reactivity, calculated from a sample collected in 2020
+#'
+#' @format A data frame with 25 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"SubTable"
+
+#' 2018 Population Norms for BPQ Sub-diaphragmatic Reactivity
+#'
+#' A dataset containing population norms for BPQ Sub-diaphragmatic Reactivity, calculated from a sample collected in 2018
+#'
+#' @format A data frame with 25 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"SubTable_2018"
+
+
+#' 2020 Population Norms for BPQ Supra-diaphragmatic Reactivity
+#'
+#' A dataset containing population norms for BPQ Supra-diaphragmatic Reactivity, calculated from a sample collected in 2020
+#'
+#' @format A data frame with 61 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"SupTable"
+
+#' 2018 Population Norms for BPQ Supra-diaphragmatic Reactivity
+#'
+#' A dataset containing population norms for BPQ Supra-diaphragmatic Reactivity, calculated from a sample collected in 2018
+#'
+#' @format A data frame with 61 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"SupTable_2018"
+
+
+#' 2020 Population Norms for BPQ Very Short Form
+#'
+#' A dataset containing population norms for BPQ Very Short Form, calculated from a sample collected in 2020
+#'
+#' @format A data frame with 49 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"VSFTable"
+
+#' 2018 Population Norms for BPQ Very Short Form
+#'
+#' A dataset containing population norms for BPQ Very Short Form, calculated from a sample collected in 2018
+#'
+#' @format A data frame with 49 rows and 3 variables:
+#' \describe{
+#'   \item{raw}{raw score, numeric}
+#'   \item{percents}{percentile score, numeric}
+#'   \item{tScores}{t scores, numeric}
+#'   ...
+#' }
+"VSFTable_2018"
